47646700f5232311d178ed2c660bfcbd1d4d210e
Ranlot <ranlot75@gmail.com>
2016-04-05 16:01:04 +0200

    all moves

--- Localization/localizerMotion.py
+++ Localization/localizerMotion.py
@@ -1,7 +1,7 @@
 import itertools
 #------------------------------------------------
 def interLeaver(locationGrid):
-	return map(lambda row: zip(colors[row], locationGrid[row]), range(sizeOfGrid))
+	return map(lambda row: zip(worldMap[row], locationGrid[row]), range(sizeVertical))
 
 def deInterLeaver(coLocationGrid):
 	return [map(lambda cell: cell[1], row) for row in coLocationGrid]
@@ -14,34 +14,70 @@ def measurementProcessor(measurement, probSensorIsRight):
 
 #using numpy array would look more simple, yes...???
 def locationNormalizer(locationMatrix):
-	normalizingConstant = sum(map(sum, locationMatrix))
-	return [map(lambda cell: cell / normalizingConstant, locationMatrix[row]) for row in range(sizeOfGrid)]	
+	normalizingConstant, sizeVertical = sum(map(sum, locationMatrix)), len(locationMatrix)
+	if normalizingConstant == 0.0:	raise ValueError('worldMap probably inconsistent with measurements & moves')
+	return [map(lambda cell: cell / normalizingConstant, locationMatrix[row]) for row in range(sizeVertical)]	
 
-def localize(measurement, coLoc):
-	newLocation = locationNormalizer(map(measurementProcessor(measurement, sensor_right), coLoc))
-	return interLeaver(newLocation)
-#------------------------------------------------
-sizeOfGrid = 3
+def localize(measurement, probSensorIsRight, coLoc):
+	newLocation = locationNormalizer(map(measurementProcessor(measurement, probSensorIsRight), coLoc))
+	return newLocation
 
-colors = [['G', 'G', 'G'],
-          ['G', 'R', 'G'],
-          ['G', 'G', 'G']]
+def shift(locationRow, n, direction):
+	if direction == 'right':	pivot = len(locationRow) - n
+	else:				pivot = n
+	return locationRow[pivot::] + locationRow[:pivot:]
 
-assert sum([len(colors[x]) == sizeOfGrid for x in range(sizeOfGrid)]) == sizeOfGrid
+def horizontalMove(locationRow, direction, probMoveIsSuccessful):
+	failedMove = map(lambda cell: (1 - probMoveIsSuccessful) * cell, locationRow)
+	successfulMove = map(lambda cell: probMoveIsSuccessful * cell, shift(locationRow, 1, direction))
+	return [x + y for (x, y) in zip(failedMove, successfulMove)]
+
+def locationFlipper(locationMatrix):
+	return map(list, zip(*locationMatrix))
+	
+def executeMove(move, locationMatrix):
+	if move == 'stay':
+		return locationMatrix
+	if move == 'left' or move == 'right':		
+		return locationNormalizer(map(lambda locationRow: horizontalMove(locationRow, move, probMoveIsSuccessful), locationMatrix))
+	elif move == 'up' or move == 'down':
+		transposeLocationMatrix, transposeMove = locationFlipper(locationMatrix), 'right' if move == 'down' else 'left'
+		transposedResult = locationNormalizer(map(lambda locationRow: horizontalMove(locationRow, transposeMove, probMoveIsSuccessful), transposeLocationMatrix))
+		return map(list, zip(*transposedResult))
+	else:
+		raise ValueError('move not supported')
+
+def locationPrinter(locationMatrix, debug=False):
+	if debug:	locationPrint = interLeaver(locationMatrix)
+	else:		locationPrint = locationMatrix
+	for cell in locationPrint:	print cell
+	print '\n'
+
+def step(move, measurement, locationMatrix):
+	makeTheMove = executeMove(move, locationMatrix)
+	posteriorLocation = localize(measurement, probSensorIsRight, interLeaver(makeTheMove))
+	locationPrinter(posteriorLocation, True)
+	return posteriorLocation
 #------------------------------------------------
-sensor_right = 1.0
-p_move = 1.0
+worldMap = [['R', 'G', 'G', 'R', 'R'], ['R', 'R', 'G', 'R', 'R'], ['R', 'R', 'G', 'G', 'R'], ['R', 'R', 'R', 'R', 'R']]
 #------------------------------------------------
-flatInitProb = 1 / (float(sizeOfGrid) ** 2)
-flatRow = [flatInitProb for cell in range(sizeOfGrid)]
-flatLandscape = list(itertools.repeat(flatRow, sizeOfGrid))
+sizeHorizontal, sizeVertical = len(worldMap[0]), len(worldMap)
+assert [len(row) for row in worldMap] == [sizeHorizontal] * sizeVertical
+#------------------------------------------------
+probSensorIsRight = 0.7
+probMoveIsSuccessful = 0.8
+#------------------------------------------------
+flatInitProb = 1 / (float(sizeHorizontal * sizeVertical))
+flatRow = [flatInitProb for cell in range(sizeHorizontal)]
+flatLandscape = list(itertools.repeat(flatRow, sizeVertical))
 #------------------------------------------------
 measurements = ['R']
 motions = [[0, 0]]
 #------------------------------------------------
-initialCoLocation = interLeaver(flatLandscape)
+locationPrinter(flatLandscape, True)
+l0 = step('stay', 'G', flatLandscape)
+l1 = step('right', 'G', l0)
+l2 = step('down', 'G', l1)
+l3 = step('down', 'G', l2)
+l4 = step('right', 'G', l3)
 #------------------------------------------------
-result = localize(measurements[0], initialCoLocation)
-print result
-#------------------------------------------------
-
