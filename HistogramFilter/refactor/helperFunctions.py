00d95486acf5f2a15715fe73995eb4011e357228
Ranlot <ranlot75@gmail.com>
2016-04-14 13:11:30 +0200

    removed class refactoring for histogram filter

--- HistogramFilter/refactor/helperFunctions.py
+++
@@ -1,6 +0,0 @@
-def measurementProcessor(measurement, probSensorIsRight):
-	def processRow(colocationRow):
-		return map(lambda cell: probSensorIsRight * cell[1] if cell[0] == measurement else (1 - probSensorIsRight) * cell[1], colocationRow)
-	return processRow
-
-
