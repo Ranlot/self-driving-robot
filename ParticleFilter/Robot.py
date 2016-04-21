561e308e7b2749d75039e34388375315d9558286
Ranlot <ranlot75@gmail.com>
2016-04-21 13:48:24 +0200

    ususal non-sense

--- ParticleFilter/Robot.py
+++ ParticleFilter/Robot.py
@@ -1,77 +1,72 @@
-#simplified version of class provided in online course
-
-from math import *
 import random
-from scipy.stats import norm
+from math import pi, cos, sin, sqrt, exp, hypot
 from operator import mul
+from scipy.stats import norm
 
 class Robot:
-
-	def __init__(self, obj):
-		self.landmarks = obj['landmarks']
-		self.world_size = obj['worldSize']
-       		self.x = random.random() * self.world_size
-       		self.y = random.random() * self.world_size
-        	self.orientation = random.random() * 2.0 * pi
-        	self.forward_noise = 0.0
-        	self.turn_noise    = 0.0
-        	self.sense_noise   = 5.0
-		    
-    	def set(self, obj):
-		new_x = obj['x']
-		new_y = obj['y']
-		new_orientation = obj['orientation']
-        	if new_x < 0 or new_x >= self.world_size:
-            		raise ValueError, 'X coordinate out of bound'
-        	if new_y < 0 or new_y >= self.world_size:
-            		raise ValueError, 'Y coordinate out of bound'
-        	if new_orientation < 0 or new_orientation >= 2 * pi:
-            		raise ValueError, 'Orientation must be in [0..2pi]'
-        	self.x = float(new_x)
-       		self.y = float(new_y)
-        	self.orientation = float(new_orientation)
+    def __init__(self, obj):
+	self.landmarks = obj['landmarks']
+	self.world_size = obj['worldSize']
+        self.x = random.random() * self.world_size
+        self.y = random.random() * self.world_size
+        self.orientation = random.random() * 2.0 * pi
+        self.forward_noise = 0.0;
+        self.turn_noise    = 0.0;
+        self.sense_noise   = 0.0;
     
-    	def set_noise(self, obj):
-		self.forward_noise = float(obj['forward'])
-		self.turn_noise = float(obj['turn'])
-		self.sense_noise = float(obj['sense'])
-
-
-	def __distanceRobotToLandmark(self, obj):
-		noiseLessEuclideanDistance = hypot(obj['robotX'] - obj['landmarkX'], obj['robotY'] - obj['landmarkY']) 
-		return noiseLessEuclideanDistance + random.gauss(0.0, self.sense_noise)
-
-	def sense(self):
-		robotAndLandmarks = [{'robotX': self.x, 'robotY': self.y, 'landmarkX': landmark[0], 'landmarkY': landmark[1]} for landmark in self.landmarks]
-		return map(self.__distanceRobotToLandmark, robotAndLandmarks)
-
-	def move(self, obj):
-		turn = obj['turn']
-		forward = obj['forward']
-        	if forward < 0:
-            		raise ValueError, 'Robot cant move backwards'         
-        
-        	# turn, and add randomness to the turning command
-        	orientation = self.orientation + float(turn) + random.gauss(0.0, self.turn_noise)
-       		orientation %= 2 * pi
-        
-        	# move, and add randomness to the motion command
-        	dist = float(forward) + random.gauss(0.0, self.forward_noise)
-        	x = self.x + (cos(orientation) * dist)
-        	y = self.y + (sin(orientation) * dist)
-        	x %= self.world_size    # cyclic truncate
-        	y %= self.world_size
-        
-		self.x = x
-		self.y = y
-		self.orientation = orientation
-		
-	def getParticleWeight(self, robotMeasurement):
-		particleMeasurement = map(lambda landmark: hypot(self.x - landmark[0], self.y - landmark[1]), self.landmarks)
-		gaussianMismatchWeight = norm.pdf(x=particleMeasurement, loc=robotMeasurement, scale=self.sense_noise)
-		return reduce(mul, gaussianMismatchWeight, 1)
+    def set(self, new_x, new_y, new_orientation):
+        if new_x < 0 or new_x >= self.world_size:
+            raise ValueError, 'X coordinate out of bound'
+        if new_y < 0 or new_y >= self.world_size:
+            raise ValueError, 'Y coordinate out of bound'
+        if new_orientation < 0 or new_orientation >= 2 * pi:
+            raise ValueError, 'Orientation must be in [0..2pi]'
+        self.x = float(new_x)
+        self.y = float(new_y)
+        self.orientation = float(new_orientation)
     
-    	def __repr__(self):
-        	return '[x=%.6s y=%.6s heading=%.6s]' % (str(self.x), str(self.y), str(self.orientation))
+    
+    def set_noise(self, obj):
+	self.forward_noise = float(obj['forward'])
+	self.turn_noise = float(obj['turn'])
+	self.sense_noise = float(obj['sense'])
+   
+    def __distanceRobotToLandmark(self, obj):
+	noiseLessEuclideanDistance = hypot(obj['robotX'] - obj['landmarkX'], obj['robotY'] - obj['landmarkY']) 
+	return noiseLessEuclideanDistance + random.gauss(0.0, self.sense_noise)
 
+    def sense(self):
+	robotAndLandmarks = [{'robotX': self.x, 'robotY': self.y, 'landmarkX': landmark[0], 'landmarkY': landmark[1]} for landmark in self.landmarks]
+	return map(self.__distanceRobotToLandmark, robotAndLandmarks)
+
+    def move(self, obj):
+	turn = obj['turn']
+	forward = obj['forward']
+        if forward < 0:
+            raise ValueError, 'Robot cant move backwards'         
+        
+        # turn, and add randomness to the turning command
+        orientation = self.orientation + float(turn) + random.gauss(0.0, self.turn_noise)
+        orientation %= 2 * pi
+        
+        # move, and add randomness to the motion command
+        dist = float(forward) + random.gauss(0.0, self.forward_noise)
+        x = self.x + (cos(orientation) * dist)
+        y = self.y + (sin(orientation) * dist)
+        x %= self.world_size    # cyclic truncate
+        y %= self.world_size
+        
+        # set particle
+        res = Robot({'landmarks':self.landmarks, 'worldSize':self.world_size})
+        res.set(x, y, orientation)
+        res.set_noise({'forward': self.forward_noise, 'turn': self.turn_noise, 'sense': self.sense_noise})
+        return res
+	
+    def getParticleWeight(self, robotMeasurement):
+	particleMeasurement = map(lambda landmark: hypot(self.x - landmark[0], self.y - landmark[1]), self.landmarks)
+	gaussianMismatchWeight = norm.pdf(x=particleMeasurement, loc=robotMeasurement, scale=self.sense_noise)
+	return reduce(mul, gaussianMismatchWeight, 1.0)
+
+    def __repr__(self):
+        return '[x=%.6s y=%.6s orient=%.6s]' % (str(self.x), str(self.y), str(self.orientation))
 
--- ParticleFilter/runRobot.py
+++ ParticleFilter/runRobot.py
@@ -1,29 +1,53 @@
+# Please only modify the indicated area below!
+
 from math import *
+import random
 from Robot import Robot
-
-'''
-def eval(r, p):
-	sum = 0.0;
-    	for i in range(len(p)): # calculate mean error
-        	dx = (p[i].x - r.x + (self.world_size/2.0)) % self.world_size - (self.world_size/2.0)
-        	dy = (p[i].y - r.y + (self.world_size/2.0)) % self.world_size - (self.world_size/2.0)
-        	err = sqrt(dx * dx + dy * dy)
-        	sum += err
-    	return sum / float(len(p))
-'''
-
+import numpy as np
 
 landmarks  = [[20.0, 20.0], [80.0, 80.0], [20.0, 80.0], [80.0, 20.0]]
 worldSize = 100.0
+numbOfParticles = 1000
+
+def eval(r, p):
+	sum = 0.0;
+    	for i in range(len(p)): # calculate mean error
+        	dx = (p[i].x - r.x + (worldSize/2.0)) % worldSize - (worldSize/2.0)
+        	dy = (p[i].y - r.y + (worldSize/2.0)) % worldSize - (worldSize/2.0)
+        	err = sqrt(dx * dx + dy * dy)
+        	sum += err
+    	return sum / float(len(p))
+
+
+#def cyclicDistance(coord1, coord2):
+#	return (coord1 - coord2 + (worldSize/2.0)) % worldSize - (worldSize/2.0)
+
+#def myEval(robot, particles):
+#	dx = map(lambda particle: particle.x, robot.x, particles)
+#	dy = map(lambda particle: particle.y, robot.y, particles)
+#	[x*x for x in dx]
+#	[x*x for x in dy]	
 
 robot = Robot({'landmarks':landmarks, 'worldSize':worldSize})
+robot = robot.move({'turn': 0.1, 'forward': 5.0})
+robotMeasurement = robot.sense()
 
-#robot.set_noise({'forward': 5, 'turn': 0.1, 'sense': 5})
-robot.set({'x': 30, 'y': 50, 'orientation': pi / 2})
+particles = [Robot({'landmarks':landmarks, 'worldSize':worldSize}) for particle in range(numbOfParticles)]
+[particle.set_noise({'forward': 0.05, 'turn': 0.05, 'sense': 5.0}) for particle in particles]
+
+print eval(robot, particles)
+
+for timeStamp in range(10):
+	robot = robot.move({'turn': 0.1, 'forward': 5.0})
+	robotMeasurement = robot.sense()
+
+	particles = [particle.move({'turn': 0.1, 'forward': 5.0}) for particle in particles]
+
+	particleWeights = [particle.getParticleWeight(robotMeasurement) for particle in particles]
+	particleWeights = map(lambda weight: weight / sum(particleWeights), particleWeights)
+
+	particles = np.random.choice(a=particles, size=len(particles), replace=True, p=particleWeights)
+	print eval(robot, particles)
+
 print robot
-robot.move({'turn': - pi / 2, 'forward': 15})
-print robot
-print robot.sense()
-robot.move({'turn': - pi / 2, 'forward': 10})
-print robot
-print robot.sense()
+print particles[:20]
