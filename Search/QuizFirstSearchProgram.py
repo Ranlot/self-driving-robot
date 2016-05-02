e86fc4b21c0111f3197d269328dd267d9b734c44
Ranlot <ranlot75@gmail.com>
2016-05-02 11:37:38 +0200

    first implementation of a star with manhattan heuristic

--- Search/QuizFirstSearchProgram.py
+++ Search/QuizFirstSearchProgram.py
@@ -1,40 +1,16 @@
 from operator import add, sub
 from numpy import abs
 
-'''
-grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 1, 0],
-        [0, 0, 0, 0, 1, 0],
-        [1, 0, 0, 0, 1, 0]]
-'''
-
-grid = [[0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0]]
-
-nrows, ncols = len(grid), len(grid[0])
-
-metaMoves = [('^', [-1, 0]), ('<', [0, -1]), ('v', [1, 0]), ('>', [0, 1])]
-init = [0, 0]
-goal = [4, 5]
-
-cost = 1
-
-currentStatus = [{'weight': 0, 'position': init}]
-
-allowedMoves = [move[1] for move in metaMoves]
-listOfVisitedSites = [currentStatus[0]['position']]
-
 #-----------------------------------------------------------------
 #-----------------------------------------------------------------
-extractXandY = lambda position: (position[0], position[1])
-
 def gridShower(grid):
 	for row in grid:	print row
 	print '\n'
+
+def manhattanDistance(startPosition, targetPosition):
+	return abs(startPosition[0] - targetPosition[0]) + abs(startPosition[1] - targetPosition[1])		
+
+extractXandY = lambda position: (position[0], position[1])
 
 def validBoundaryChecker(position):
 	xpos, ypos = extractXandY(position)
@@ -56,12 +32,12 @@ def expandPositions(status):
 	currentPosition = status['position']
 	currentWeight = status['weight']
 	boundaryAndWallFilter = neighborFinder(currentPosition)
-	previouslyVisitedFilter = filter(lambda position: position not in listOfVisitedSites, boundaryAndWallFilter)
-	return [{'weight': currentWeight + cost, 'position': position} for position in previouslyVisitedFilter]
+	previouslyVisitedFilter = filter(lambda position: position not in closedSet, boundaryAndWallFilter)
+	return [{'weight': currentWeight + naiveManhanttanHeuristics[position[0]][position[1]], 'position': position} for position in previouslyVisitedFilter]
 
 def processStatus(status):
 	visitedSites = expandPositions(status)
-	[listOfVisitedSites.append(site['position']) for site in visitedSites]
+	[closedSet.append(site['position']) for site in visitedSites]
 	return visitedSites
 
 def findBestNeighborAndMove(weightGrid, position):
@@ -72,63 +48,79 @@ def findBestNeighborAndMove(weightGrid, position):
 	bestMove = [move[0] for move in metaMoves if move[1] == map(sub, position, bestNeighbor)]
 	return {'bestNeighbor':bestNeighbor, 'bestMove':bestMove}
 
-
-def manhattanDistance(startPosition, targetPosition):
-	return abs(startPosition[0] - targetPosition[0]) + abs(startPosition[1] - targetPosition[1])		
-	
-
+def expandNodes(openSet):
+	sortedOpenSet = sorted(openSet, key=lambda x: x['weight'], reverse=False)
+	bestCandidateForExansion = sortedOpenSet[0]
+	xpos, ypos = extractXandY(bestCandidateForExansion['position'])
+	weightGrid[xpos][ypos] = bestCandidateForExansion['weight']
+	expandedSites = expandPositions(bestCandidateForExansion)
+	[closedSet.append(site['position']) for site in expandedSites]
+	newOpenSet = openSet[1:] + expandedSites
+	newOpenSet = sorted(newOpenSet, key=lambda x: x['weight'], reverse=False)
+	return newOpenSet
 #-----------------------------------------------------------------
 #-----------------------------------------------------------------
+
+'''
+grid = [[0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 1, 0],
+        [1, 0, 0, 0, 1, 0]]
+'''
+
+grid = [[0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0]]
+
+nrows, ncols = len(grid), len(grid[0])
+
+#-----------------------------------------------------------------
+metaMoves = [('^', [-1, 0]), ('<', [0, -1]), ('v', [1, 0]), ('>', [0, 1])]
+init, goal = [0, 0], [4, 5]
+#-----------------------------------------------------------------
+allManhattanHeuristics = map(lambda x: manhattanDistance(x, goal), [(x, y) for x in range(nrows) for y in range(ncols)])
+naiveManhanttanHeuristics = zip(*[iter(allManhattanHeuristics)] * ncols)
+#naiveManhanttanHeuristics = [[1] * ncols for row in range(nrows)]
+gridShower(naiveManhanttanHeuristics)
+#-----------------------------------------------------------------
+openSet = [{'weight': naiveManhanttanHeuristics[init[0]][init[1]], 'position': init}]
+
+allowedMoves = [move[1] for move in metaMoves]
+closedSet = [openSet[0]['position']]
+
 
 weightGrid = [[-1]*6 for x  in range(5)]
 
-xInit, yInit = extractXandY(listOfVisitedSites[0])
-weightGrid[xInit][yInit] = currentStatus[0]['weight']
+xInit, yInit = extractXandY(closedSet[0])
+weightGrid[xInit][yInit] = openSet[0]['weight']
 
-finalSolvability = 'Undecided'
+finalSolvability = False
 
+print openSet
 while True:
-	#print currentStatus
-
-	currentStatus = sorted(currentStatus, key=lambda x: x['weight'])
-	currentStatus = sum(map(processStatus, currentStatus), [])
-
-	for status in currentStatus:
-		
-		xpos, ypos = extractXandY(status['position'])
-		weightGrid[xpos][ypos] = status['weight']
-
-	if True in [status['position'] == goal for status in currentStatus]:
-		#print 'Success', currentStatus		
+	openSet = expandNodes(openSet)
+	print openSet
+	if len(openSet) == 0:
+		print 'Failure to find a path'	
+		break
+	if True in [status['position'] == goal for status in openSet]:
 		finalSolvability = True
 		break
 
-	if len(currentStatus) == 0:
-		print 'Failure to find a path'
-		finalSolvability = False
-		break	
 
 gridShower(weightGrid)
 
 #start backwards propagation
 if finalSolvability:
-
 	pathGrid = [[' ']*6 for x  in range(5)]
 	pathGrid[goal[0]][goal[1]] = '*'
-
 	backTrackPosition = goal
 	while backTrackPosition != init:
-	#for mv in range(10):
 		bestPath = findBestNeighborAndMove(weightGrid, backTrackPosition)
 		backTrackPosition, backTrackMove = bestPath['bestNeighbor'], bestPath['bestMove'][0]
 		pathGrid[backTrackPosition[0]][backTrackPosition[1]] = backTrackMove
-
 	gridShower(pathGrid)
-
-
-allManhattanHeuristics = map(lambda x: manhattanDistance(x, goal), [(x, y) for x in range(nrows) for y in range(ncols)])
-naiveManhanttanHeuristics = zip(*[iter(allManhattanHeuristics)] * ncols)
-
-gridShower(naiveManhanttanHeuristics)
-
 
